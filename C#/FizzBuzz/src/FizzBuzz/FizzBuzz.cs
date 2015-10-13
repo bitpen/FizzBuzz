15971158535d6adc3c50cf6657836a8cdafa1260
bitpen <bitpen@gmail.com>
2015-10-12 20:39:04 -0400

    Initial Commit
    Initial Repository creation and commit of base code.

---
+++ C#/FizzBuzz/src/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private const int _fizzTrigger = 3;
+        private const int _buzzTrigger = 5;
+
+        private const string _fizzText = "Fizz";
+        private const string _buzzText = "Buzz";
+
+        private readonly int _startInclusive;
+        private readonly int _endInclusive;
+        private int _currentPosition;
+
+
+        public FizzBuzz(int startInclusive, int endInclusive)
+        {
+            _startInclusive = startInclusive;
+            _endInclusive = endInclusive;
+        }
+
+        public void Execute()
+        {
+            Reset();
+            Loop();
+        }
+
+        private void Reset()
+        {
+            _currentPosition = _startInclusive - 1;
+        }
+
+        private void Loop()
+        {
+            while (!IsComplete())
+            {
+                PrintNextLine();
+            }
+        }
+
+        private bool IsComplete()
+        {
+            return _currentPosition >= _endInclusive;
+        }
+
+        private void PrintNextLine()
+        {
+            IncrementPosition();
+            PrintCurrentValue();
+        }
+
+        private void IncrementPosition()
+        {
+            ++_currentPosition;
+        }
+
+        private void PrintCurrentValue()
+        {
+            Console.WriteLine(GetComputedOutput());
+        }
+
+        private string GetComputedOutput()
+        {
+            var output = GetFizzValue() + GetBuzzValue();
+            if (String.IsNullOrEmpty(output))
+            {
+                output = GetDefaultValue();
+            }
+            return output;
+        }
+
+        private string GetFizzValue()
+        {
+            if(IsFizzTrigger() || (IsFizzTrigger() && IsBuzzTrigger()))
+            {
+                return _fizzText;
+            }
+            return string.Empty;
+        }
+
+        private string GetBuzzValue()
+        {
+            if(IsBuzzTrigger() || (IsFizzTrigger() && IsBuzzTrigger()))
+            {
+                return _buzzText;
+            }
+            return string.Empty;
+        }
+
+        private string GetDefaultValue()
+        {
+            return _currentPosition.ToString();
+        }
+
+        private bool IsFizzTrigger()
+        {
+            return _currentPosition % _fizzTrigger == 0;
+        }
+
+        private bool IsBuzzTrigger()
+        {
+            return _currentPosition % _buzzTrigger == 0;
+        }
+
+        
+    }
+}
---
+++ C#/FizzBuzz/src/FizzBuzz/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            CheckArguments(args);
+            PrintFizzBuzz(args);
+        }
+
+        private void CheckArguments(string[] args)
+        {
+            new FizzBuzzArguments(args).AreValid();
+        }
+
+        private void PrintFizzBuzz(string[] args)
+        {
+            var fizzBuzzArguments = new FizzBuzzArguments(args);
+            if (fizzBuzzArguments.AreValid())
+            {
+                new FizzBuzz(fizzBuzzArguments.StartNumber(), fizzBuzzArguments.EndNumber()).Execute();
+            }
+        }
+
+        private class FizzBuzzArguments
+        {
+            private readonly string[] _args;
+
+            public FizzBuzzArguments(string [] args)
+            {
+                _args = args ?? new string[0];
+            }
+
+            public bool AreValid()
+            {
+                return IsArgumentCountCorrect()
+                            && IsStartArgumentValid()
+                            && IsEndArgumentValid();
+            }
+
+            public int StartNumber()
+            {
+                return GetAsInt(_args[0]);
+            }
+
+            public int EndNumber()
+            {
+                return GetAsInt(_args[1]);
+            }
+
+            private bool IsArgumentCountCorrect()
+            {
+                return _args.Length == 2;
+            }
+
+            private bool IsStartArgumentValid()
+            {
+                return IsStartBeforeEnd() && IsPositiveNumber(GetAsInt(_args[0]));
+            }
+
+            private bool IsEndArgumentValid()
+            {
+                return IsStartBeforeEnd() && IsPositiveNumber(GetAsInt(_args[1]));
+            }
+
+            private bool IsStartBeforeEnd()
+            {
+                return GetAsInt(_args[0]) <= GetAsInt(_args[1]);
+            }
+
+            private bool IsPositiveNumber(int number)
+            {
+                return number > 0;
+            }
+
+            private int GetAsInt(string number)
+            {
+                return Int32.Parse(number);
+            }
+        }
+
+
+    }
+}
